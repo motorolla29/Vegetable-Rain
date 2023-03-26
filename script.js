253a79d0d1e18753edd49f2ae4b38acbffd000fc
motorolla29 <eutyou@gmail.com>
2023-03-26 16:15:25 +0300

    with rotation

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     
     Raindrop.prototype.update = function () {
-        this.x += this.hVelocity;
+        this.x += this.hVelocity/2;
         this.y += this.velocity;
 
         if (this.isOffscreen()) {
@@ -40,24 +40,29 @@ window.addEventListener('DOMContentLoaded', () => {
         this.size = getRandomValue(6, 20);
     
         this.x = getRandomValue(-200, screenSize.WIDTH*1.3);
-        this.y = getRandomValue(-200, -this.size*5);
+        this.y = getRandomValue(-300, -this.size*10);
     
-        this.velocity = this.size / 5;
-        this.hVelocity = -this.size / 10;
+        this.velocity = this.size/5;
+        this.hVelocity = -this.size/8;
     };
 
     // Огурцы
     var Cucumber = function () {
         Raindrop.call(this);
-        this.size = getRandomValue(20, 25);
     };
 
     Cucumber.prototype = Object.create(Raindrop.prototype);
 
     Cucumber.prototype.render = function (ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle * Math.PI / 180);
+        ctx.translate(-this.x, -this.y);
+        
         ctx.fillStyle = 'green';
         ctx.beginPath();
         ctx.ellipse(this.x, this.y, this.size, this.size * 3, 0, Math.PI * 2, false);
+        ctx.lineWidth = 5;
         ctx.stroke();
         ctx.fill();
         ctx.closePath();
@@ -77,47 +82,61 @@ window.addEventListener('DOMContentLoaded', () => {
         ctx.stroke();
         ctx.strokeStyle = 'black';
         ctx.closePath();
+        
+        ctx.restore();
     };
 
     Cucumber.prototype.update = function() {
         Raindrop.prototype.update.call(this);
-        this.angle += 0.01;
+        this.angle += 0.3;
     };
 
     Cucumber.prototype._reset = function() {
         Raindrop.prototype._reset.call(this);
-        this.angle = getRandomValue(0, Math.PI * 2);
+        this.size = getRandomValue(20, 45);
+        this.angle = getRandomValue(-45, 45);
     };
 
     // Томаты
     var Tomato = function () {
         Raindrop.call(this);
+
     };
     
     Tomato.prototype = Object.create(Raindrop.prototype);
 
     Tomato.prototype.render = function (ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle * Math.PI / 180);
+        ctx.translate(-this.x, -this.y);
+
         ctx.fillStyle = '#b33319';
         ctx.beginPath();
         ctx.ellipse(this.x, this.y, this.size*3, this.size*3.4, Math.PI/2, 0, 2 * Math.PI);
         ctx.closePath();
         ctx.fill();
+        ctx.lineWidth = 3;
         ctx.stroke();
         ctx.beginPath();
         ctx.fillStyle = '#137815';
         ctx.ellipse(this.x-this.size, this.y-this.size*4.2, this.size*1.4, this.size/1.5, Math.PI/3, 0, 2 * Math.PI);
         ctx.ellipse(this.x+this.size/5, this.y-this.size*3.8, this.size*0.8, this.size/3, -Math.PI/3, 0, 2 * Math.PI);
+        ctx.lineWidth = 5;
         ctx.stroke();
         ctx.fill();
+        
+        ctx.restore();
     };
     Tomato.prototype.update = function() {
         Raindrop.prototype.update.call(this);
-        this.angle += 0.01;
+        this.angle += 0.2;
     };
 
     Tomato.prototype._reset = function() {
         Raindrop.prototype._reset.call(this);
-        this.angle = getRandomValue(0, Math.PI * 2);
+        this.size = getRandomValue(15, 25);
+        this.angle = getRandomValue(-90, 0);
     };
 
     // Картошка
@@ -129,11 +148,17 @@ window.addEventListener('DOMContentLoaded', () => {
     Potato.prototype = Object.create(Raindrop.prototype);
 
     Potato.prototype.render = function (ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle * Math.PI / 180);
+        ctx.translate(-this.x, -this.y);
+
         ctx.fillStyle = '#3d342a';
         ctx.beginPath();
         ctx.ellipse(this.x, this.y, this.size*3, this.size*4, Math.PI, 0, 2 * Math.PI);
         ctx.closePath();
         ctx.fill();
+        ctx.lineWidth = 3;
         ctx.stroke();
         ctx.beginPath();
         ctx.lineWidth = 3;
@@ -148,15 +173,18 @@ window.addEventListener('DOMContentLoaded', () => {
         ctx.moveTo(this.x, this.y+this.size*2);
         ctx.lineTo(this.x-this.size/1.5, this.y+this.size*2);
         ctx.stroke();
+        
+        ctx.restore();
     };
     Potato.prototype.update = function() {
         Raindrop.prototype.update.call(this);
-        this.angle += 0.01;
+        this.angle -= 0.1;
     };
 
     Potato.prototype._reset = function() {
         Raindrop.prototype._reset.call(this);
-        this.angle = getRandomValue(0, Math.PI * 2);
+        this.size = getRandomValue(20, 25);
+        this.angle = getRandomValue(0, 90);
     };
 
     // Морковки
@@ -168,6 +196,11 @@ window.addEventListener('DOMContentLoaded', () => {
     Carrot.prototype = Object.create(Raindrop.prototype);
 
     Carrot.prototype.render = function (ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle * Math.PI / 180);
+        ctx.translate(-this.x, -this.y);
+
         ctx.beginPath();
         ctx.fillStyle = '#e67a07';
         ctx.moveTo(this.x, this.y);
@@ -180,6 +213,7 @@ window.addEventListener('DOMContentLoaded', () => {
         ctx.lineTo(this.x+this.size/1.5, this.y+this.size);
         ctx.moveTo(this.x, this.y+this.size*2);
         ctx.lineTo(this.x-this.size/3, this.y+this.size*2);
+        ctx.lineWidth = 3;
         ctx.stroke();
         ctx.closePath();
         ctx.fillStyle = '#49ba0b';
@@ -195,18 +229,22 @@ window.addEventListener('DOMContentLoaded', () => {
         ctx.lineTo(this.x+this.size*0.7, this.y-this.size*2);
         ctx.lineTo(this.x, this.y);
         ctx.fill();
+        ctx.lineWidth = 2;
         ctx.stroke();
         ctx.closePath();
+        
+        ctx.restore();
     };
     Carrot.prototype.update = function() {
         Raindrop.prototype.update.call(this);
-        this.angle += 0.01;
+        this.angle -= 0.2;
     };
 
     Carrot.prototype._reset = function() {
         Raindrop.prototype._reset.call(this);
-        this.angle = getRandomValue(0, Math.PI * 2);
-        this.size = getRandomValue(20, 30);
+        this.size = getRandomValue(50, 55);
+        this.angle = getRandomValue(-30, 30);
+        
     };
 
     var cleanupFrame = function (ctx) {
@@ -226,9 +264,9 @@ window.addEventListener('DOMContentLoaded', () => {
     
     var setup = function() {
         var DROPS = 100;
-        var CUCUMBER_RATIO = 0.2;
-        var TOMATO_RATIO = 0.2;
-        var POTATO_RATIO = 0.2;
+        var CUCUMBER_RATIO = 0.15;
+        var TOMATO_RATIO = 0.15;
+        var POTATO_RATIO = 0.15;
         var CARROT_RATIO = 0.15;
         var canvas = document.querySelector('#rain');
         var ctx = canvas.getContext('2d');
